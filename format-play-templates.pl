1fcd759497b509d4255776f92149457243120256
Hans Poo <hans@welinux.cl>
2017-06-09 14:04:53 -0400

    Translate to English

--- format-play-templates.pl
+++ format-play-templates.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
 
+#
+# Indent play/groovy tags when using play framework 1.x templates.
+#
 while (<>) {
 
 	if (m:#\{/(\w+):) {
 		$initTag = pop @stack;
 		if ($initTag ne $1) {
-			print STDERR "Linea $. Tag de cierre $1 desemparejado, se esperaba el hermano de $initTag\n";
+			print STDERR "Line $., unpaired closing tag $1, expected closing tag of $initTag\n";
 		}
 	}
 	print "\t" x scalar @stack;
