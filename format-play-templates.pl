52e4a683cd094b5a1e94793ee274f5dd217a3ecf
Hans Poo <hans@welinux.cl>
2018-03-07 15:47:06 -0300

    Support for start and end tag in one line

--- format-play-templates.pl
+++ format-play-templates.pl
@@ -3,14 +3,22 @@
 #
 # Indent play/groovy tags when using play framework 1.x templates.
 #
-while (<>) {
+LINEA: while (<>) {
 
+	if (m:#\{(\w+).*?\}.*#\{/\1\}:) {
+		print "\t" x scalar @stack;
+		print $_;
+		next LINEA;
+	} 
+	
+	
 	if (m:#\{/(\w+):) {
 		$initTag = pop @stack;
 		if ($initTag ne $1) {
 			print STDERR "Line $., unpaired closing tag $1, expected closing tag of $initTag\n";
 		}
 	}
+
 	print "\t" x scalar @stack;
 	print $_;
 
